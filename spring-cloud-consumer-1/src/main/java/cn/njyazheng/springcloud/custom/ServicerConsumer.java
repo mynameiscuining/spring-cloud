54a34220c803c470e3cd71f11f3c4045eac245d3
cuining <cuining@asiainfo-sec.com>
2019-04-21 22:06:27 +0800

    Zuul代理上传

--- spring-cloud-consumer-1/src/main/java/cn/njyazheng/springcloud/custom/ServicerConsumer.java
+++ spring-cloud-consumer-1/src/main/java/cn/njyazheng/springcloud/custom/ServicerConsumer.java
@@ -3,10 +3,13 @@ package cn.njyazheng.springcloud.custom;
 import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 public class ServicerConsumer {
@@ -14,7 +17,7 @@ public class ServicerConsumer {
     private RestTemplate restTemplate;
     @Value("${provider.get.name}")
     private  String url;
-    //EurekaClient≤ªƒ‹ π”√‘⁄◊¢Ω‚ @PostConstruct method or in a @Scheduled method
+    //EurekaClient‰∏çËÉΩ‰ΩøÁî®Âú®Ê≥®Ëß£ @PostConstruct method or in a @Scheduled method
     @Autowired
     private EurekaClient eurekaClient;
 
@@ -23,4 +26,11 @@ public class ServicerConsumer {
         return restTemplate.getForObject(url+1,String.class);
     }
 
+    @PostMapping(value = "/upload")
+    public String upload(@RequestParam(value = "file",required = true) MultipartFile multipartFile) throws IOException {
+        byte[] bytes=multipartFile.getBytes();
+        File file=new File(multipartFile.getOriginalFilename());
+        FileCopyUtils.copy(bytes,file);
+        return file.getAbsolutePath();
+    }
 }
