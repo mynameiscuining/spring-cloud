dbda5711c07eec7f7bd8f50a06a2dc6f58837758
cuining <cuining@asiainfo-sec.com>
2019-04-21 22:23:30 +0800

    Zuul禁用自带的过滤器

--- spring-cloud-gateway-zuul-1/src/main/java/cn/njyazheng/springcloud/ZuulApplication1.java
+++ spring-cloud-gateway-zuul-1/src/main/java/cn/njyazheng/springcloud/ZuulApplication1.java
@@ -3,6 +3,7 @@ package cn.njyazheng.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
 //默认使用的是Apache HTTP Client
@@ -13,6 +14,8 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 //通过本主机ip和端口+其他微服务的application.name(默认)/uri可以代理访问例子如下
 //http://192.168.217.1:7413/provider(app)/user(uri)
 @EnableZuulProxy
+//EnableZuulServer比EnableZuulProxy轻量,没有做很多事情,不包括任何反向代理的特性,很多设置,需要自己配置,几乎没见到这种场景
+//@EnableZuulServer
 public class ZuulApplication1 {
 
 	public static void main(String[] args) {
