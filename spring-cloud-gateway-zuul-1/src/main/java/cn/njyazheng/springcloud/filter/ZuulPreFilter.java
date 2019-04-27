73b4d934d3a259ad503dea689355a15ee37dcb73
cuining <cuining@asiainfo-sec.com>
2019-04-27 15:39:45 +0800

    zuul过滤器

--- spring-cloud-gateway-zuul-1/src/main/java/cn/njyazheng/springcloud/filter/ZuulPreFilter.java
+++ spring-cloud-gateway-zuul-1/src/main/java/cn/njyazheng/springcloud/filter/ZuulPreFilter.java
@@ -7,7 +7,12 @@ import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 
 import javax.servlet.http.HttpServletRequest;
 
-
+/**
+ * 1.pre：此种过滤器在请求被路由之前执行，显然这种过滤器可以用来过滤请求（白黑名单）、安全验证等；
+ * 2.routing：此种过滤器复制将请求路由到具体的微服务上；
+ * 3.post：此种过滤器在请求被路由到微服务之后执行，可以用来统计用户行为、响应客户端等；
+ * 4.error：如果上面三种过滤器发生了错误，则执行此过滤器
+ */
 public class ZuulPreFilter extends ZuulFilter {
 
     //设置pre的类型
